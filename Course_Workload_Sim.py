d4e94c37cbb6d428a0401430607a6a3b9d8e8b55
Shreenandan Rajarathnam <shreenandan94@yahoo.com>
2017-12-05 11:36:02 -0600

    Initial program structure outline
    Initial program structure outline

---
+++ Course_Workload_Sim.py
@@ -0,0 +1,40 @@
+#MIT License
+#
+#Copyright (c) 2017 Shreenandan Rajarathnam
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#___________________________________________________________________
+#
+#Simulation of Course Workload and Academic Pressure for U of I Students
+
+class AcademicTask:
+    '''
+        A class that represents 1 academic task (assignment, project, research paper deliverable, exam and etc.) in a week
+    '''
+
+class AcademicWeek:
+    '''
+        A class that represents 1 academic week (for a course) in a semester.
+    '''
+
+class Course:
+    '''
+        A class that represents 1 course taken by a student in a semester.
+    '''
+
+class Student:
+    '''
+        A class that represents the student.
+    '''
+
+# no. of credits, no. of
+
+def main():
+
+if __name__ == "__main__":
+    main()
