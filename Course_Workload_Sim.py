c10bbe904a7eab184719f61524311058448694bf
Shreenandan Rajarathnam <shreenandan94@yahoo.com>
2017-12-05 21:14:13 -0600

    Few more lines of code (added __init__())
    Added basic __init__() for the classes.

--- Course_Workload_Sim.py
+++ Course_Workload_Sim.py
@@ -14,27 +14,52 @@
 
 class AcademicTask:
     '''
-        A class that represents 1 academic task (assignment, project, research paper deliverable, exam and etc.) in a week
+        A class that represents 1 academic task (assignment, project, research paper deliverable, exam and etc.) in a week.
     '''
+    def __init__(self, type_of_task: str, difficulty_level: int, no_of_hours_of_work: int):
+        self._type_of_task = type_of_task
+        self._difficulty_level = difficulty_level
+        self._no_of_hours_of_work = no_of_hours_of_work
 
 class AcademicWeek:
     '''
         A class that represents 1 academic week (for a course) in a semester.
     '''
+    #_tasks = {}
+    
+    def __init__(self, week_no: int):
+        self._week_no = week_no
+
+    def get_week_no(self):
+        return self._week_no
 
 class Course:
     '''
         A class that represents 1 course taken by a student in a semester.
     '''
+    
+    _academic_weeks = {}
+    
+    def __init__(self, course_name: str, course_code: str):
+        self._course_name = course_name
+        self._course_code = course_code
+
+        for i in range(14): # 14 is the number of weeks in a semester (can be changed to a different value if needed)
+            self._academic_weeks.update({i:AcademicWeek(i)})
+            print(self._academic_weeks[i].get_week_no())
 
 class Student:
     '''
         A class that represents the student.
     '''
 
-# no. of credits, no. of
+    def __init__(self, student_name: str, netID: str, UIN: str):
+        self._student_name = student_name
+        self._netID = netID
+        self._UIN = UIN
 
 def main():
+    c_obj = Course('CourseName','CourseCode')
 
 if __name__ == "__main__":
     main()
